60693c4a169f456d5ca05f2218ae8e900186fe99
Ori Bermudez <ori.bermudez@outlook.com>
2018-04-11 16:34:24 -0500

    Search bar and transaction display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MaterializeModule } from 'angular2-materialize';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
